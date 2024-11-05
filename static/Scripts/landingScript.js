05d11a5e5263f655086e602cec708c9b614a1905
DeltaYankee86 <d_yang19@u.pacific.edu>
2024-11-04 22:05:40 -0800

    Created function to detect user agent and set appropriate download link
    (cherry picked from commit 05f50a59ad0f879bb1df5932a38a47e02be71f91)

--- static/Scripts/landingScript.js
+++ static/Scripts/landingScript.js
@@ -2,3 +2,24 @@
 function testFunction(){
     console.log("I'M ALIVE!!!");
 }
+
+// Detect browser & set appropriate download link
+function setDownloadLink(){
+    let downloadButton = document.querySelector("#button-container a");
+
+    // Sets the user's agent
+    let userAgent = navigator.userAgent;
+
+    // Verify browser in use & set the link
+    if(userAgent.includes("Chrome")){
+        downloadButton.href = ""    // path to Chrome extension -- TO BE ADDED
+    } else if (userAgent.includes("Firefox")){
+        downloadButton.href = ""    // path to Firefox extension -- TO BE ADDED
+    } else if (userAgent.includes("Safari")){
+        downloadButton.href = ""    // path to Safari extension -- TO BE ADDED
+    } else {
+        downloadButton.href = ""    // default link -- TO BE ADDED
+    }
+}
+
+window.onload = setDownloadLink;
